3b117baec523e6adbc63e802c0cd22fefcf2791c
Mirna-Tarek <mirnatarek111@gmail.com>
2023-10-05 21:42:33 +0200

    onboarding- stateful screen

---
+++ lib/features/onBoarding/presentation/views/widgets/model_onboarding.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class OnBoardingModel{
+  final String image;
+  final String title;
+  final String subTitle;
+  final Color bgColor;
+  final double height;
+
+  OnBoardingModel({
+  required this.image,
+  required this.title,
+  required this.subTitle,
+  required this.bgColor,
+  required this.height
+});
+}
