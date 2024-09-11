ecc57928ba66dfff8bf38e1e9e4eff52effb7b7b
Mirna Tarek <mirnatarek60@gmail.com>
2024-09-12 01:32:52 +0300

    Delete my_app directory

--- my_app/lib/cubits/weather_cubit/weatherStates.dart
+++
@@ -1,8 +0,0 @@
-abstract class WeatherState{}
-class weatherInitial extends WeatherState{}
-
-class weatherLoading extends WeatherState{}
-
-class weatherSuccess extends WeatherState{}
-
-class weatherFailure extends WeatherState{}
