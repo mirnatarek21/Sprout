d9504494224ed6978080820c911ba6d49f667db1
Mirna Tarek <mirnatarek60@gmail.com>
2024-09-12 01:36:12 +0300

    Delete onboarding directory

--- onboarding/lib/utils/assets.dart
+++
@@ -1,7 +0,0 @@
-
-class AssetsData {
-  static const image1 = 'assets/images/peno.png';
-  static const image2 = 'assets/images/peno2.png';
-  static const image3 = 'assets/images/peno4.png';
-
-}
--- onboarding/lib/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kMainColor = Color(0xffFF592C),
-    kBackgroundColor = Color(0xffFCFCFC);
-const Duration kNavDuration = Duration(milliseconds: 300);
