b9ff1842e09a2b52bf304f36dd753f20092350ff
Mirna Tarek <mirnatarek60@gmail.com>
2024-10-05 21:03:58 +0300

    Delete sprout/lib directory

--- sprout/lib/utils/home_widgets/spacing.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-SizedBox verticalSpace(double height) => SizedBox(height: height.h);
-SizedBox horizontalspace(double width) => SizedBox(width: width.w);
